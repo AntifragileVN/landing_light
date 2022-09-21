a070030f91f3788c7691e762f854a1ba24f91fcc
Vitaliti Dudar <dudarvitalik1@gmail.com>
2022-09-21 21:07:56 +0300

    Add project file

---
+++ JS/JS/ex_3-switch.js
@@ -0,0 +1,15 @@
+let user = "user"
+switch(user){
+    case "superAdmin":
+        console.log("В вас є права - superAdmin");
+
+     case "moderator":
+        console.log("В вас є права - модератора");
+
+     case "Admin":
+        console.log("В вас є права - адміна");
+
+     case "user":
+         console.log("В вас є права - користувача");
+
+}
