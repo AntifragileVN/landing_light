674bdf002639c7468f0821788b28eb03ab6ea1c0
Vitaliti Dudar <dudarvitalik1@gmail.com>
2022-09-30 23:07:41 +0300

    Add animation to burger menu

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -3,14 +3,30 @@ const headerMobileMenu = document.querySelector(".header__mobile-menu");
 const headerBurgerClose = document.querySelector(".header__close-btn ");
 
 const handleBurgerBtnClick = () => {
-  headerBurger.style.display = "none";
-  headerMobileMenu.style.display = "block";
-};
-
-const handleBurgerCloseClick = () => {
-  headerBurger.style.display = "block";
-  headerMobileMenu.style.display = "none";
+  // headerBurger.classList.add();
+  headerBurger.style.cssText = `
+    opacity: 0;
+    pointer-events: none;
+    `;
+  headerMobileMenu.style.cssText = `
+    opacity: 1;
+    pointer-events: all;
+    transition: 0.5s;
+    `;
 };
 
 headerBurger.addEventListener("click", handleBurgerBtnClick);
+
+const handleBurgerCloseClick = () => {
+  headerBurger.style.cssText = `
+    opacity: 1;
+    pointer-events: all;
+    `;
+  headerMobileMenu.style.cssText = `
+  opacity: 0; 
+  pointer-events: none;
+  transition: 0.5s;
+  `;
+};
+
 headerBurgerClose.addEventListener("click", handleBurgerCloseClick);
