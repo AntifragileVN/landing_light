c5ea1bb53264d2dd1e523b48fa78475e497b5bf8
Vitaliti Dudar <dudarvitalik1@gmail.com>
2023-11-28 10:49:19 +0200

    Optimize photo and icons. Work with media query

--- src/assets/scripts/body-elements.js
+++
@@ -1,8 +0,0 @@
-// ************Get element in console by click************
-const body = document.querySelector(".body");
-
-body.onclick = (e) => {
-  if (e.target != ".order") {
-    console.log("cleack on =  ", e.target);
-  }
-};
--- src/assets/scripts/order.js
+++
@@ -1,9 +0,0 @@
-// Order select
-const order = document.querySelector(".order");
-
-const handleOrderClick = (e) => {
-  const eTarget = e.target.value;
-  console.log("e target ==", eTarget);
-};
-
-order.addEventListener("click", handleOrderClick);
